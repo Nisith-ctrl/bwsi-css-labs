c9fe7a2111879222882ba66484c77ab3e3d56868
Nisith-ctrl <nisithabeywardana@gmail.com>
2026-03-17 21:15:40 -0400

    Added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,8 @@ def main():
     name = "Nisith Abeywardana" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    
+    print("Hi everyone! My name is Nisith and I am a Sophomore from Tampa, Florida. One fun fact about me is that I play the piano. I hope to learn a lot for this course and apply my knowledge in many different areas.")
 
 if __name__ == "__main__":
     main()
