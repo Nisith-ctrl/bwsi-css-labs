7abe397a08a6327ee93a899781af3a452cbcd39d
Nisith-ctrl <nisithabeywardana@gmail.com>
2026-03-18 11:55:31 -0400

    Fixed code and added tests
    Fixed the critical errors that were present in lab_1c.py and lab_1d.py. Also created test files as tests_1c.py and tests_1d.py for these two files. Modified run_test.yml to have Github Actions test the two files.

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -10,6 +10,15 @@ Derived from LeetCode problem: https://leetcode.com/problems/maximum-subarray/ (
 
 # TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
 def max_subarray_sum(nums: list[int]) -> int:
+
+    # Input sanitization
+    if not nums:
+        raise ValueError("Input list is invalid. Please provide a valid list of integers.")
+    
+    for num in nums:
+        if not isinstance(num, int):
+            raise ValueError("Input list is invalid. Please provide a valid list of integers.")
+
     """
     Function that takes in a list of integers and returns the maximum sum of any contiguous subarray.
 
@@ -20,20 +29,24 @@ def max_subarray_sum(nums: list[int]) -> int:
         int: The maximum sum of any contiguous subarray.
     """
 
+        
     max_current = max_global = nums[0]
     
-    for num in nums:
+    for num in nums[1:]:
         max_current = max(num, max_current + num)
-        if max_current < max_global:
+        if max_current > max_global:
             max_global = max_current
             
     return max_global
 
-# Example usage:
 def main():
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
-    result = max_subarray_sum(nums)
-    print(f"Maximum subarray sum: {result}")
+    nums = []
+    
+    try:
+        result = max_subarray_sum(nums)
+        print(f"Maximum subarray sum: {result}")
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
