87fa53cb620455770978b9f39194d40a81d23e11
Nisith-ctrl <nisithabeywardana@gmail.com>
2026-03-17 21:24:11 -0400

    Change robot speed variable
    Changed robot speed variable from 5 to 8

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,7 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+This is to simulate a change made on a robot: robot_speed = 8 # m/s
 """
 
 def main():
@@ -13,7 +13,7 @@ def main():
     name = "Nisith Abeywardana" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-    
+
     print("Hi everyone! My name is Nisith and I am a Sophomore from Tampa, Florida. One fun fact about me is that I play the piano. I hope to learn a lot for this course and apply my knowledge in many different areas.")
 
 if __name__ == "__main__":
